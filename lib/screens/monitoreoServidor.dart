d992f4be0dc6d450f4be81ef62dd76cceb170756
Michael Vargas S <michaelvargas47@hotmail.com>
2023-04-17 23:43:53 -0600

    Monitoreo Server incompleto

--- lib/screens/monitoreoServidor.dart
+++ lib/screens/monitoreoServidor.dart
@@ -15,132 +15,105 @@ class _MonitoreoServidorState extends State<MonitoreoServidor> {
 
   void initState() {
     monitoreoServidor = Servidor_Service.monitoreoServidor(StaticC.idServidor);
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // return Center(
-    //   child: TextButton(
-    //     onPressed: () async {
-    //       setState(() {
-    //         _asynCall = true;
-    //       });
-    //       monitoreoServidor =
-    //           Servidor_Service.monitoreoServidor(StaticC.idServidor);
-    //       setState(() {
-    //         _asynCall = false;
-    //       });
-    //     },
-    //     child: Text("Ver Servidores"),
-    //   ),
-    // );
-
     return Scaffold(body: porcentajes());
   }
 
   Widget porcentajes() {
-    return Scaffold(
-      appBar: new AppBar(
-        title: new Text("Circular Percent Indicators"),
-      ),
-      body: Center(
-        child: ListView(children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(15.0),
-            child: new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new CircularPercentIndicator(
-                  radius: 45.0,
-                  lineWidth: 4.0,
-                  percent: 0.10,
-                  center: new Text("10%"),
-                  progressColor: Colors.red,
-                  footer: new Text("CPU",
-                      style: new TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 15.0)),
-                ),
-                new Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                ),
-                new CircularPercentIndicator(
-                  radius: 45.0,
-                  lineWidth: 4.0,
-                  percent: 0.30,
-                  center: new Text("30%"),
-                  progressColor: Colors.orange,
-                  footer: new Text("RAM",
-                      style: new TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 15.0)),
-                ),
-                new Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                ),
-                new CircularPercentIndicator(
-                  radius: 45.0,
-                  lineWidth: 4.0,
-                  percent: 0.60,
-                  center: new Text("60%"),
-                  progressColor: Colors.yellow,
-                  footer: new Text("DISCO",
-                      style: new TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 15.0)),
-                ),
-                new Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                ),
-              ],
+    return FutureBuilder(
+      future: monitoreoServidor,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: new AppBar(
+              title: new Text(
+                  "Monitoreo del Servidor : " + "\n" + snapshot.data!.nombre),
             ),
-          ),
-          Container(
-            height: 500,
-            width: 250,
-            padding: EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(53, 255, 255, 255),
-              borderRadius: BorderRadius.circular(21),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.2),
-                  blurRadius: 10,
-                  spreadRadius: 2,
-                ),
-              ],
+            body: Center(
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(15.0),
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new CircularPercentIndicator(
+                          radius: 45.0,
+                          lineWidth: 4.0,
+                          percent: snapshot.data!.cpu / 10,
+                          center: new Text(snapshot.data!.cpu.toString() + "%"),
+                          progressColor: Colors.red,
+                          footer: new Text("CPU",
+                              style: new TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15.0)),
+                        ),
+                        new Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        ),
+                        new CircularPercentIndicator(
+                          radius: 45.0,
+                          lineWidth: 4.0,
+                          percent: 0.30,
+                          center: new Text("30%"),
+                          progressColor: Colors.orange,
+                          footer: new Text("RAM",
+                              style: new TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15.0)),
+                        ),
+                        new Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        ),
+                        new CircularPercentIndicator(
+                          radius: 45.0,
+                          lineWidth: 4.0,
+                          percent: 0.60,
+                          center: new Text("60%"),
+                          progressColor: Colors.yellow,
+                          footer: new Text("DISCO",
+                              style: new TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15.0)),
+                        ),
+                        new Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Color.fromARGB(255, 243, 242, 243),
+                  ),
+                  Container(
+                    height: 60,
+                    color: Color.fromARGB(255, 54, 94, 206),
+                    child: Center(
+                        child: Text('Nombre del Servidor:' +
+                            "\n" +
+                            snapshot.data!.nombre)),
+                  ),
+                  Container(
+                    height: 60,
+                    color: Color.fromARGB(255, 54, 171, 206),
+                    child: Center(
+                        child: Text('Fecha Monitoreo:' +
+                            "\n" +
+                            snapshot.data!.fechaMonitoreo.toString())),
+                  ),
+                ],
+              ),
             ),
-            child: _detalle(),
-          ),
-        ]),
-      ),
+          );
+        } else {
+          //entra aqui si no hay datos
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
-  } //fin porcentajes
-
-  Widget _detalle() {
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        Container(
-          height: 50,
-          color: Color.fromARGB(255, 41, 137, 216),
-          child: Center(child: Text('Codigo del Servidor:')),
-        ),
-        Container(
-          height: 50,
-          color: Color.fromARGB(255, 32, 168, 192),
-          child: Center(child: Text('Nombre: ')),
-        ),
-        Container(
-          height: 50,
-          color: Color.fromARGB(255, 41, 137, 216),
-          child: Center(child: Text('Descripcion:')),
-        ),
-        Container(
-          height: 50,
-          color: Color.fromARGB(255, 32, 168, 192),
-          child: Center(child: Text('Usuario que Administra:')),
-        ),
-      ],
-    );
-  }
+  } //fin metodo porcentajes
 }//fin class
