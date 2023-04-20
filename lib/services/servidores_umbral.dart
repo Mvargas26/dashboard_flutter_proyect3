99313aaa251731957ab1e79fa2f7fd29e9e1021c
Michael Vargas S <michaelvargas47@hotmail.com>
2023-04-19 23:59:45 -0600

    Se corrige error del mensaje Monica

--- lib/services/servidores_umbral.dart
+++ lib/services/servidores_umbral.dart
@@ -5,62 +5,66 @@ import 'package:http/http.dart' as http;
 import '/models/umbral.dart';
 import '/models/servidor.dart';
 
-
 class Umbral_Service {
   //VARIABLES
   static String _baseURL = 'http://10.0.2.2:5021/';
 
-
-
   //*** Metodo para obtener Seervidor
-static Future<List<ServidorNombre>> getNombre() async {
-  var url = Uri.parse(_baseURL + "servidorUmbral1");
-  final response = await http.get(url);
-  if (response.statusCode == 200) {
-    List<dynamic> jsonList = json.decode(response.body);
-    List<ServidorNombre> servidorList =
-        jsonList.map((e) => ServidorNombre.fromJson(e)).toList();
-    return servidorList;
-  } else {
-    throw Exception("Fallo");
+  static Future<List<ServidorNombre>> getNombre() async {
+    var url = Uri.parse(_baseURL + "servidorUmbral1");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      List<dynamic> jsonList = json.decode(response.body);
+      List<ServidorNombre> servidorList =
+          jsonList.map((e) => ServidorNombre.fromJson(e)).toList();
+      return servidorList;
+    } else {
+      throw Exception("Fallo");
+    }
   }
-}
-
 
 //*** Metodo para Crear parametro
-static Future<bool> createUmbral(Umbral c) async{
-  var url =Uri.parse(_baseURL+"paramSensibilidad");
-  final response= await http.post(url, headers:<String, String>{
-    'Content-Type':'application/json; charset=UTF-8'
-  },
-  body: jsonEncode(c.toJson()));
-  if(response.statusCode==201){
-    return true;
-  }else{
-    return false;
+  static Future<bool> createUmbral(Umbral c) async {
+    var url = Uri.parse(_baseURL + "paramSensibilidad");
+    final response = await http.post(url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: jsonEncode(c.toJson()));
+    if (response.statusCode == 201) {
+      return true;
+    } else {
+      return false;
+    }
   }
-}
 
 //*** Metodo para Crear servidor
-static Future<bool> createServidor(ServidoresR c) async{
-  var url =Uri.parse(_baseURL+"Servidor");
-  final response= await http.post(url, headers:<String, String>{
-    'Content-Type':'application/json; charset=UTF-8'
-  },
-  body: jsonEncode(c.toJson()));
-  if(response.statusCode==201){
-    return true;
-  }else{
-    return false;
+  static Future<bool> createServidor(ServidoresR c) async {
+    var url = Uri.parse(_baseURL + "Servidor");
+    final response = await http.post(url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: jsonEncode(c.toJson()));
+    if (response.statusCode == 201 || response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
   }
-}
-
 
 //*** Metodo para Eliminar parametro
-static Future<bool> eliminarUmbral(String server, String umbral, String componente ) async {
-    var url =
-        Uri.parse(_baseURL + "paramSensibilidad/"+server+"/"+umbral+"/"+componente+"");
-   
+  static Future<bool> eliminarUmbral(
+      String server, String umbral, String componente) async {
+    var url = Uri.parse(_baseURL +
+        "paramSensibilidad/" +
+        server +
+        "/" +
+        umbral +
+        "/" +
+        componente +
+        "");
+
     final response = await http.delete(url);
     if (response.statusCode == 200) {
       return true;
@@ -69,9 +73,7 @@ static Future<bool> eliminarUmbral(String server, String umbral, String componen
     }
   }
 
-
-
-   //*** Metodo para EDITAR parametro
+  //*** Metodo para EDITAR parametro
   static Future<bool> ediparametro(Umbral c) async {
     var url = Uri.parse(_baseURL + 'paramSensibilidad');
 
@@ -87,7 +89,6 @@ static Future<bool> eliminarUmbral(String server, String umbral, String componen
       return false;
     }
   } //fn editarparametro
-
 
 //*** Metodo para EDITAR servidor
   static Future<bool> editarServidor(ServidoresR c) async {
@@ -106,12 +107,10 @@ static Future<bool> eliminarUmbral(String server, String umbral, String componen
     }
   } //fn sevidor
 
-
   //*** Metodo para Eliminar parametro
-static Future<bool> eliminarServidor(String server ) async {
-    var url =
-        Uri.parse(_baseURL + "del_Servicios+ /"+server+"");
-   
+  static Future<bool> eliminarServidor(String server) async {
+    var url = Uri.parse(_baseURL + "del_Servicios+ /" + server + "");
+
     final response = await http.delete(url);
     if (response.statusCode == 200) {
       return true;
@@ -120,28 +119,16 @@ static Future<bool> eliminarServidor(String server ) async {
     }
   }
 
-
-static Future<List<ServidorNombre>> buscarServidor(String nombre) async {
-  var url = Uri.parse(_baseURL + "servidorUmbral/"+nombre+"");
-  final response = await http.get(url);
-  if (response.statusCode == 200) {
-    List<dynamic> jsonList = json.decode(response.body);
-    List<ServidorNombre> servidorList =
-        jsonList.map((e) => ServidorNombre.fromJson(e)).toList();
-    return servidorList;
-  } else {
-    throw Exception("Fallo");
+  static Future<List<ServidorNombre>> buscarServidor(String nombre) async {
+    var url = Uri.parse(_baseURL + "servidorUmbral/" + nombre + "");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      List<dynamic> jsonList = json.decode(response.body);
+      List<ServidorNombre> servidorList =
+          jsonList.map((e) => ServidorNombre.fromJson(e)).toList();
+      return servidorList;
+    } else {
+      throw Exception("Fallo");
+    }
   }
 }
-
-}
-
-
-
-
-
-
-
-
-
-
